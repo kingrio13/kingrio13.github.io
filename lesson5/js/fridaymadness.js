c701317924cd8f263b427ea3641295edfaff7d31
kingrio13 <kingrio.miranda@gmail.com>
2020-02-07 10:57:30 +0800

    first commit

---
+++ lesson5/js/fridaymadness.js
@@ -0,0 +1,9 @@
+//
+
+var mad_date = new Date();
+var mad_weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+//var mad_num = mad_date.getDay();
+var mad_num=5;
+if (mad_num!=5){
+    document.getElementById("fridaybanner").style="display:none;"
+}
--- lesson5/js/menujs.js
+++ lesson5/js/menujs.js
@@ -1,6 +1,18 @@
 const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
+
+
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
+  hambutton.classList.toggle("changex");
+  
+  if (hambutton.classList.contains("changex")) {
+    hambutton.innerHTML="X";
+  } else {
+    hambutton.innerHTML="&#9776; Menu";
+  }
+  
+  
+
 }
