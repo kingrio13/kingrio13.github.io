14fa42aab743779f176e5cd7cbd1aedb82468d73
kingrio13 <kingrio.miranda@gmail.com>
2020-02-08 11:25:03 +0800

    first commit

--- lesson5/js/fridaymadness.js
+++ lesson5/js/fridaymadness.js
@@ -1,9 +1,8 @@
 //
 
 var mad_date = new Date();
-var mad_weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-//var mad_num = mad_date.getDay();
-var mad_num=5;
+var mad_num = mad_date.getDay();
+//var mad_num=5; //testing purposes
 if (mad_num!=5){
     document.getElementById("fridaybanner").style="display:none;"
 }
