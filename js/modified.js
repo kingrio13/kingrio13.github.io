fbe353a3e5ea419820391d879f2a87a77181991d
kingrio13 <kingrio.miranda@gmail.com>
2020-02-12 07:38:45 +0800

    first commit

--- js/modified.js
+++ js/modified.js
@@ -1,2 +1,10 @@
-let nLastModif = document.lastModified;
-    document.getElementById('lastupdated').innerHTML="Last Updated: "+ nLastModif;
+let nLastModify = new Date(document.lastModified);
+
+//footer date in format Wednesday, 24 July 2020 (including comma), uses JS
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var nLastModif= days[nLastModify.getDay()] + ", " +months[nLastModify.getMonth()] +" "+ nLastModify.getFullYear();
+
+document.getElementById('lastupdated').innerHTML="Last Updated: "+ nLastModif;
