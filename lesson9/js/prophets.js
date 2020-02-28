4678e7a4169adea2e4e1dae82ab4e025585b21cc
kingrio13 <kingrio.miranda@gmail.com>
2020-02-28 19:48:22 +0800

    prophets

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,17 +5,27 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject); 
 
     const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let image = document.createElement('img');
+      let birthdate= document.createElement('span');
+      let birthplace= document.createElement('span');
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      birthdate.textContent="Date of Birthdate: "+prophets[i].birthdate;
+      birthplace.textContent="Place of Birth: "+prophets[i].birthplace;
+      //birthdate
+      //birthplace
+      let alttext= prophets[i].name + ' ' + prophets[i].lastname + "-" + prophets[i].order;
       image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt',alttext);
 
       card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(birthplace);
       card.appendChild(image);
       document.querySelector('div.cards').appendChild(card);
      
