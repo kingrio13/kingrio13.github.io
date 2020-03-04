ebc7bc938435d4dbbc024bcc8d6b7c580a58fc93
kingrio13 <kingrio.miranda@gmail.com>
2020-03-04 10:41:41 +0800

    lesson 9

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -16,18 +16,21 @@ fetch(requestURL)
 
         let section = document.createElement('section');
 
-        
+
+        let townContent= document.createElement('div');
+
         let townName= document.createElement('h2');
         let townMotto= document.createElement('span');
         let yearFounded= document.createElement('span');
         let currentPopulation= document.createElement('span');
         let averageRainfall= document.createElement('span');
-
-
         let townImg= document.createElement('img');
 
 
+        townContent.setAttribute('class',"towntextcontainer");
+
         townName.textContent = towns[i].name;
+        
         townMotto.textContent = towns[i].motto;
         yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
         currentPopulation.textContent ="Population: " + towns[i].currentPopulation;
@@ -36,16 +39,20 @@ fetch(requestURL)
 
         let alttext=towns[i].photo;  
         // since there is no image url i will use default for now
-        townImg.setAttribute('src', "images/gallery/placeholder.png"); 
+
+        let imgsrc="images/"+ alttext.toLowerCase();
+        townImg.setAttribute('src', imgsrc); 
         townImg.setAttribute('alt',alttext);
 
-
-        section.appendChild(townName);
+        
+        townContent.appendChild(townName);
         townMotto.setAttribute('class',"townmotto");
-        section.appendChild(townMotto);
-        section.appendChild(yearFounded);
-        section.appendChild(currentPopulation);
-        section.appendChild(averageRainfall);
+        townContent.appendChild(townMotto);
+        townContent.appendChild(yearFounded);
+        townContent.appendChild(currentPopulation);
+        townContent.appendChild(averageRainfall);
+
+        section.appendChild(townContent);
         section.appendChild(townImg);
 
 
