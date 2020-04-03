2f8eca69ece7f340df5f410b112c2e3718952b50
kingrio13 <kingrio.miranda@gmail.com>
2020-04-03 08:49:02 +0800

    final project backbone

---
+++ salmonriver/js/menujs.js
@@ -0,0 +1,19 @@
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+
+
+function toggleMenu() {
+  document.querySelector(".mainmenu_ul").classList.toggle("responsive");
+  hambutton.classList.toggle("changex");
+  
+  if (hambutton.classList.contains("changex")) {
+    hambutton.innerHTML="X";
+  } else {
+    hambutton.innerHTML="&#9776; Menu";
+  }
+  
+  
+
+}
---
+++ salmonriver/js/webfont.js
@@ -0,0 +1,5 @@
+ WebFont.load({
+   google: {
+     families: ['Advent Pro','sans-serif','Overlock SC','cursive']
+   }
+ });
