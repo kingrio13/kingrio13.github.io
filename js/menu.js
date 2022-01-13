dda43910825de0cc1c8433700c3cbada522ecd7e
kingrio13 <kingrio.miranda@gmail.com>
2022-01-13 14:14:28 +0800

    partial portfolio

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+    var menu_icon = document.getElementById("menu-icon");
+var menus = document.getElementById("menu");
+
+
+
+menu_icon.classList.toggle("open");
+ menus.classList.toggle("active");
+
+}              
+           
