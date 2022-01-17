41008a389ef9a5e06c768b556a0b6172236b3157
kingrio13 <kingrio.miranda@gmail.com>
2022-01-17 23:20:57 +0800

    finalize pageup link

--- js/portfolio_observer.js
+++ js/portfolio_observer.js
@@ -6,6 +6,8 @@ const skills = document.querySelectorAll("ul.webskills li");
 const myskills_title = document.querySelectorAll("#myskills .container span");
 const contactus_linked = document.querySelectorAll(".contactus_linked ul li");
 
+const goup= document.querySelectorAll(".go_up");
+
 const appearOptions = {
   threshold: 0,
   rootMargin: "10px"
@@ -17,6 +19,7 @@ const appearOnScroll = new IntersectionObserver(function(
   appearOnScroll
 ) {
   entries.forEach(entry => {
+
     if (!entry.isIntersecting) {
       return;
     } else {
@@ -29,6 +32,10 @@ const appearOnScroll = new IntersectionObserver(function(
         entry.target.style.opacity= '1';
         entry.target.style.transition= `opacity ${entry.target.dataset.delay} ease-in`;
     }
+
+ 
+
+
 
   });
 },
@@ -55,3 +62,25 @@ featureimage.forEach(featureimage => {
 contactus_linked.forEach(contactus_linked => {
     appearOnScroll.observe(contactus_linked);
   });
+
+
+
+
+
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+
+    
+  const mybutton = document.querySelector(".go_up");
+
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+      mybutton.style.opacity= '1';
+      //check the properties
+    } else {
+      mybutton.style.opacity= '0';
+    }
+  }
+  
